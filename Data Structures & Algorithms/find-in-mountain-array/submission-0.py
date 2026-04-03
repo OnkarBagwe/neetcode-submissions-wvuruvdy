3876bf004aa1e945b42307ff8e8c3c2418ec34b9
OnkarBagwe <59908015+OnkarBagwe@users.noreply.github.com>
2026-04-02 17:45:53 -0700

    Add: find-in-mountain-array - submission-0

---
+++ Data Structures & Algorithms/find-in-mountain-array/submission-0.py
@@ -0,0 +1,50 @@
+class Solution:
+    def findInMountainArray(self, target: int, mountainArr: 'MountainArray') -> int:
+        length = mountainArr.length()
+
+        #find peak
+        l = 1
+        r = length - 2
+        while l <= r:
+            m = l + (r-l)//2
+            left = mountainArr.get(m-1)
+            mid = mountainArr.get(m)
+            right = mountainArr.get(m+1)
+            if left < mid < right:
+                l = m + 1
+            elif left > mid > right:
+                r = m - 1
+            else:
+                break
+        
+        peak = m
+
+        #Search left portion
+        l = 0
+        r = peak - 1
+
+        while l <= r:
+            m = l + (r-l)//2
+            val = mountainArr.get(m)
+            if val < target:
+                l = m + 1
+            elif val > target:
+                r = m - 1
+            else:
+                return m
+        
+        #Search right portion (decreasing order so opposite swap for l and r)
+        l = peak
+        r = length - 1
+
+        while l <= r:
+            m = l + (r-l)//2
+            val = mountainArr.get(m)
+            if val > target:
+                l = m + 1
+            elif val < target:
+                r = m - 1
+            else:
+                return m
+
+        return -1
