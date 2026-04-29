ebd28a1d539c2f3b412c73cd6061b73b8f992fde
OnkarBagwe <59908015+OnkarBagwe@users.noreply.github.com>
2026-04-29 08:47:08 -0700

    Add: lru-cache - submission-6

---
+++ Data Structures & Algorithms/lru-cache/submission-6.py
@@ -0,0 +1,54 @@
+class ListNode:
+    def __init__(self,key,val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.cap = capacity
+        self.left = ListNode(0,0)
+        self.right = ListNode(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def insert(self,node):
+        prev = self.right.prev
+        next = self.right
+        node.next = next
+        node.prev = prev
+        prev.next = node
+        next.prev = node
+    
+    def remove(self,node):
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = ListNode(key,value)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
