0c152342977c3c02ac2ec31ab5b82ab3af041751
OnkarBagwe <59908015+OnkarBagwe@users.noreply.github.com>
2026-05-19 18:14:32 -0700

    Add: unique-paths-ii - submission-6

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-6.py
@@ -0,0 +1,37 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        #Recursive
+        #TC: O(M*N) and SC: O(M*N)
+
+        # M = len(obstacleGrid)
+        # N = len(obstacleGrid[0])
+
+        # dp = {(M-1, N-1): 1}
+
+        # def dfs(r,c):
+        #     if r == M or c == N or obstacleGrid[r][c]:
+        #         return 0
+        #     if (r,c) in dp:
+        #         return dp[(r,c)]
+
+        #     dp[(r,c)] = dfs(r+1,c) + dfs(r,c+1)
+        #     return dp[(r,c)]
+
+        # return dfs(0,0)
+
+        #Bottom-Up
+        #TC: O(M*N) and SC: O(N)
+
+        M = len(obstacleGrid)
+        N = len(obstacleGrid[0])
+        dp = [0] * N
+        dp[N-1] = 1
+
+        for r in range(M-1,-1,-1):
+            for c in range(N-1,-1,-1):
+                if obstacleGrid[r][c]:
+                    dp[c] = 0
+                elif c + 1 < N:
+                    dp[c] = dp[c] + dp[c+1]
+        
+        return dp[0]
