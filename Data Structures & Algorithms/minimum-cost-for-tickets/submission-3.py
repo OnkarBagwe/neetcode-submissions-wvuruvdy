4bd2b2b111fb3e7f917365c67e620a40fd9f4708
OnkarBagwe <59908015+OnkarBagwe@users.noreply.github.com>
2026-05-24 23:48:10 -0700

    Add: minimum-cost-for-tickets - submission-3

---
+++ Data Structures & Algorithms/minimum-cost-for-tickets/submission-3.py
@@ -0,0 +1,42 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        #TOP-DOWN
+        # cost_map = {1: costs[0],
+        #             7: costs[1],
+        #             30: costs[2]
+        #             }
+        # dp = {}
+
+        # def dfs(i):
+        #     if i == len(days):
+        #         return 0
+        #     if i in dp:
+        #         return dp[i]
+
+        #     dp[i] = float("inf")
+        #     for d,c in cost_map.items():
+        #         j = i
+        #         while j < len(days) and days[j] < days[i] + d:
+        #             j += 1
+        #         dp[i] = min(dp[i], c + dfs(j))
+        #     return dp[i]
+
+        # return dfs(0)
+        
+        #BOTTOM-UP
+        cost_map = {1: costs[0],
+                    7: costs[1],
+                    30: costs[2]
+                    }
+        n = len(days)
+        dp = [0]*(n+1)
+
+        for i in range(n-1,-1,-1):
+            j = i
+            dp[i] = float("inf")
+            for d,c in cost_map.items():
+                j = i
+                while j < len(days) and days[j] < days[i] + d:
+                    j += 1
+                dp[i] = min(dp[i], c + dp[j])
+        return dp[0]
