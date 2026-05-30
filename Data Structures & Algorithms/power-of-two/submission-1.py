43d8da140103f93ab7377aa8f87cc323bfa2fca0
OnkarBagwe <59908015+OnkarBagwe@users.noreply.github.com>
2026-05-29 17:45:12 -0700

    Add: power-of-two - submission-1

---
+++ Data Structures & Algorithms/power-of-two/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        # if n <= 0:
+        #     return False
+
+        # while n % 2 == 0:
+        #     n >>= 1
+        
+        # return n == 1
+
+        return n > 0 and (n & (n - 1)) == 0
