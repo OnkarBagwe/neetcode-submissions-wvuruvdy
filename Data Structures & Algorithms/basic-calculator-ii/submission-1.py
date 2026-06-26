f9f360a7d733f376a87ec185b779dcd49b952746
OnkarBagwe <59908015+OnkarBagwe@users.noreply.github.com>
2026-06-26 13:21:02 -0700

    Add: basic-calculator-ii - submission-1

---
+++ Data Structures & Algorithms/basic-calculator-ii/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stk = []
+        num = 0
+        op = '+'
+        s = s.replace(' ','')
+
+        for i,ch in enumerate(s):
+            if ch.isdigit():
+                num = num*10+int(ch)
+                print(num)
+
+            if (not ch.isdigit()) or i == len(s) - 1:
+                if op == "+":
+                    stk.append(num)
+                elif op == "-":
+                    stk.append(-num)
+                elif op == "*":
+                    stk.append(stk.pop()*num)
+                else:
+                    prev = stk.pop()
+                    stk.append(int(prev/num))
+                
+                op = ch
+                num = 0
+            
+        return sum(stk)
